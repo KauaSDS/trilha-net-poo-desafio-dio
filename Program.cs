b29095fb1e9e458caf5e7979799e29a033341c75
Kauã Soares Dantas de Sousa <kaua.soares17@outlook.com>
2024-10-28 13:28:08 -0300

    FInalizando desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone");
+Smartphone ip = new Iphone("859897866", "Modelo k", "12313123", 8);
+ip.Ligar();
+ip.ReceberLigacao();
+ip.InstalarAplicativo("Instagram");
+
+Console.WriteLine();
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nk = new Nokia("94857866", "Modelo j", "22323233", 6);
+nk.Ligar();
+nk.ReceberLigacao();
+nk.InstalarAplicativo("Youtube");
